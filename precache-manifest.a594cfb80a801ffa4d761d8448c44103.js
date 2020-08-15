3f15eeae7f11e45d0aa6948101e497c05c58b21a
Garvit Anand <you@example.com>
2020-08-15 12:27:12 +0530

    Updates

---
+++ precache-manifest.a594cfb80a801ffa4d761d8448c44103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b372f37a5ba418da30819a5ef3b0d10",
+    "url": "/React_Setup_1.0/index.html"
+  },
+  {
+    "revision": "c0cc93959b12b77ffe05",
+    "url": "/React_Setup_1.0/static/css/2.d6b6d2e2.chunk.css"
+  },
+  {
+    "revision": "bfa9af2fbd2c5f342f43",
+    "url": "/React_Setup_1.0/static/css/main.c54f9495.chunk.css"
+  },
+  {
+    "revision": "c0cc93959b12b77ffe05",
+    "url": "/React_Setup_1.0/static/js/2.be0a053a.chunk.js"
+  },
+  {
+    "revision": "bfa9af2fbd2c5f342f43",
+    "url": "/React_Setup_1.0/static/js/main.caf02c89.chunk.js"
+  },
+  {
+    "revision": "149400999841e3126516",
+    "url": "/React_Setup_1.0/static/js/runtime-main.056ed53c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/React_Setup_1.0/static/media/logo.25bf045c.svg"
+  }
+]);
